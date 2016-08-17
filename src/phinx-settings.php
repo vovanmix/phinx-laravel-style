8689d6764a1b4d790b177f2f87a114323008253c
Vladimir Mikhaylovskiy <singingcode@gmail.com>
2016-08-16 20:21:44 -0700

    Update phinx-settings.php

--- src/phinx-settings.php
+++ src/phinx-settings.php
@@ -1,54 +1,72 @@
 <?php
 
-use Vovanmix\PhinxLaravelStyle\AbstractMigration;
-use Vovanmix\PhinxLaravelStyle\Table;
+// Init DotEnv
+$dotEnv = new Dotenv\Dotenv(__DIR__);
+$dotEnv->load();
 
-/**
- * Class CreateUsersTable
- */
-class CreateUsersTable extends AbstractMigration
-{
+// Use composer autoloader to load vendor classes
+require __DIR__ . '/vendor/autoload.php';
 
+if (! function_exists('env')) {
     /**
-     * Create
-     * 
-     * @return void
-     */
-    public function change()
-    {
-
-        $this->create(
-            'table_name', function (Table $table) {
-                $table->increments('id');
-                $table->timestamps();
-            }
-        );
-
-        $this->update(
-            'table_name', function (Table $table) {
-
-            }
-        );
-    }
-
-    /**
-     * Up
+     * Gets the value of an environment variable. Supports boolean, empty and null.
      *
-     * @return void
+     * @param  string  $key
+     * @param  mixed   $default
+     * @return mixed
      */
-    public function up()
+    function env($key, $default = null)
     {
+        $value = getenv($key);
 
+        if ($value === false) {
+            return value($default);
+        }
+
+        switch (strtolower($value)) {
+            case 'true':
+            case '(true)':
+                return true;
+
+            case 'false':
+            case '(false)':
+                return false;
+
+            case 'empty':
+            case '(empty)':
+                return '';
+
+            case 'null':
+            case '(null)':
+                return;
+        }
+
+//		if (Str::startsWith($value, '"') && Str::endsWith($value, '"')) {
+//			return substr($value, 1, -1);
+//		}
+
+        return $value;
     }
-
-    /**
-     * Down
-     *
-     * @return void
-     */
-    public function down()
-    {
-
-    }
-
 }
+
+return array(
+    "paths" => array(
+        "migrations" => "database/migrations"
+    ),
+    "environments" => array(
+        "default_migration_table" => "migrations",
+        "default_database" => "master",
+        "master" => array(
+            "adapter" => env("DB_ADAPTER", "mysql"),
+            "host" => env('DB_HOST'),
+            "name" => env('DB_NAME'),
+            "user" => env('DB_USER'),
+            "pass" => env('DB_PASSWORD'),
+            "port" => env('DB_PORT', 3306),
+            "unix_socket" => env('DB_SOCKET', null)
+        )
+    ),
+    "aliases" => [
+        "default" => "\\Vovanmix\\PhinxLaravelStyle\\MigrationCreator"
+    ]
+);
